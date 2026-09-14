4e589dae654ceeb5eb8db942a5dc97d73e8f810e
dmnhard <user@contoso.com>
2026-09-14 17:08:42 +0600

    Change rooms order

--- hwa_extension.js
+++ hwa_extension.js
@@ -1,4 +1,4 @@
-// ==UserScript==
+﻿// ==UserScript==
 // @name         Dungeon runner
 // @namespace    http://tampermonkey.net/
 // @version      2026-08-31_09:49
@@ -83,9 +83,9 @@
 
     const DEFAULT_ORDER = [
         //{ id: 'mixed', label: '⚡', background: 'linear-gradient(to bottom, #806104, #FFC107)', bColor: '#806104' },
-        { id: 'mixed', label: '⚡', background: 'radial-gradient(circle,#806104,#ffffff)', bColor: '#806104' },
         { id: 'water', label: '💧', background: 'linear-gradient(to bottom, #104B7A, #2196F3)', bColor: '#104B7A' },
         { id: 'earth', label: '🍀', background: 'linear-gradient(to bottom, #265828, #4CAF50)', bColor: '#265828' },
+        { id: 'mixed', label: '⚡', background: 'radial-gradient(circle,#806104,#ffffff)', bColor: '#806104' },
         { id: 'fire', label: '🔥', background: 'linear-gradient(to bottom, #7A211B, #F44336)', bColor: '#7A211B' },
     ]
 
